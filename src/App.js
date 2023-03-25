7aac512bbdf3ea6f89cba8f01fd8409c1d90d631
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-03-25 14:56:16 +0900

    [FEAT] login 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Main from "./pages/main";
+import Login from "./pages/login";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Main />
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,6 @@
+import { FaBeer } from "react-icons/fa";
+const Main = () => {
+  return <FaBeer />;
+};
+
+export default Main;
