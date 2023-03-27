4f8a88623c0f103bb17c0c3a77b6376f47517a48
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-03-27 21:57:10 +0900

    충돌

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,4 @@
 import "./main.css";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SearchBox from "../components/SearchBox/SearchBox";
 import LoginButton from "../components/login/loginButton";
